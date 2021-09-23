362e6aefbc1632f1f2010afc4b18feea9bc93e3a
Man Wing Fai <manfai106@gmail.com>
2021-09-23 16:54:59 +0800

    hotfix

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -28,7 +28,9 @@ class Media extends Model
     {
 
         $link = 'https://air.ecbento.com' . Storage::url($this->path . $this->file_name);
-        if (curl_init($link) !== false) {
+        $headers = get_headers($link);
+        $exists = stripos($headers[0],"200 OK")?true:false;
+        if ($exists) {
             return $link;
         } else {
             return str_replace('air.ecbento.com','supplier.ecbento.com',$link);
