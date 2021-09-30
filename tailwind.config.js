fa15b32b1da6404750d2459f4f0143112eced811
Man Wing Fai <manfai106@gmail.com>
2021-09-30 13:13:56 +0800

    hotfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,37 +35,33 @@ module.exports = {
         },
     },
     daisyui: {
-        themes: [
-            {
-              'mytheme': {                          /* your theme name */
-                 'primary' : '#F04E2A',           /* Primary color */
-                 'primary-focus' : '#F04E2A',     /* Primary color - focused */
-                 'primary-content' : '#ffffff',   /* Foreground content color to use on primary color */
-      
-                 'secondary' : '#179046',         /* Secondary color */
-                 'secondary-focus' : '#179046',   /* Secondary color - focused */
-                 'secondary-content' : '#ffffff', /* Foreground content color to use on secondary color */
-      
-                 'accent' : '#37cdbe',            /* Accent color */
-                 'accent-focus' : '#2aa79b',      /* Accent color - focused */
-                 'accent-content' : '#ffffff',    /* Foreground content color to use on accent color */
-      
-                 'neutral' : '#3d4451',           /* Neutral color */
-                 'neutral-focus' : '#2a2e37',     /* Neutral color - focused */
-                 'neutral-content' : '#ffffff',   /* Foreground content color to use on neutral color */
-      
-                 'base-100' : '#ffffff',          /* Base color of page, used for blank backgrounds */
-                 'base-200' : '#f9fafb',          /* Base color, a little darker */
-                 'base-300' : '#d1d5db',          /* Base color, even more darker */
-                 'base-content' : '#1f2937',      /* Foreground content color to use on base color */
-      
-                 'info' : '#2094f3',              /* Info */
-                 'success' : '#009485',           /* Success */
-                 'warning' : '#ff9900',           /* Warning */
-                 'error' : '#ff5724',             /* Error */
-              },
-            },
-          ],
+        themes: true,
+        // themes: [
+        //     {
+        //     'dsc': {
+        //         'primary': '#EA5234',
+        //         'primary-focus': '#D43616',
+        //         'primary-content': '#FFFFFF',
+        //         'secondary': '#E96D7C',
+        //         'secondary-focus': '#E24053',
+        //         'secondary-content': '#FFFFFF',
+        //         'accent': '#EEAF3A',
+        //         'accent-focus': '#E19914',
+        //         'accent-content': '#FFFFFF',
+        //         'neutral': '#1B0D22',
+        //         'neutral-focus': '#D6D6D6',
+        //         'neutral-content': '#FFFFFF',
+        //         'base-100': '#FAF7F5',
+        //         'base-200': '#EFEAE6',
+        //         'base-300': '#E7E2DF',
+        //         'base-content': '#291334',
+        //         'info': '#3C8DAD',
+        //         'success': '#01937C',
+        //         'warning': '#E1701A',
+        //         'error': '#FF5724',
+        //     },
+        //     },
+        // ],
     },
     plugins: [
         require("@tailwindcss/forms"),
