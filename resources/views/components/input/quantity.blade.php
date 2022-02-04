bc5c187d14d2663ed4b776e3681d785ad7a6f0db
Man Wing Fai <manfai106@gmail.com>
2022-02-04 14:53:07 +0800

    hotfix

--- resources/views/components/input/quantity.blade.php
+++ resources/views/components/input/quantity.blade.php
@@ -1,11 +1,11 @@
 <div class="relative">
   <select class="select pl-3 pr-10">
     <option>1</option>
-    <!-- <option>2</option>
+    <option>2</option>
     <option>3</option>
     <option>4</option>
     <option>5</option>
     <option>6</option>
-    <option>7</option> -->
+    <option>7</option>
   </select>
 </div>
