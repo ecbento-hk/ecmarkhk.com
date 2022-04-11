9106d784c6648d5dcb6e14f03e62465ce4d0e63c
Man Wing Fai <manfai106@gmail.com>
2022-04-11 15:01:49 +0800

    hotfix tnc

--- resources/views/pages/terms_and_conditions.blade.php
+++ resources/views/pages/terms_and_conditions.blade.php
@@ -3,7 +3,7 @@
 
         <div class="px-2 pb-16 lg:px-6 mx-2 grid grid-cols-12 gap-4">
 
-            <div class="w-full col-span-12 mt-6">
+            {{-- <div class="w-full col-span-12 mt-6">
                 <h1 class="text-2xl"> <strong>EC BENTO website Terms of Use</strong> </h1>
                 <p>
                     These Terms of Use govern your use of the <strong>EC BENTO</strong> website
@@ -348,7 +348,123 @@
                     © 2021 EC BENTO. All rights reserved.
                 </p>
               
-            </div>
+            </div> --}}
+
+            <section>
+<style type="text/css">
+  p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 24.0px Helvetica; color: #1e0e27}
+  p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 16.0px Helvetica; color: #1e0e27}
+  p.p3 {margin: 0.0px 0.0px 0.0px 0.0px; font: 16.0px Helvetica; color: #1e0e27; min-height: 19.0px}
+  span.s1 {font-kerning: none; background-color: #f9f5f2}
+  span.s2 {font-kerning: none}
+</style>
+<p class="p1"><span class="s1"><b>EC BENTO website Terms of Use</b></span></p>
+<p class="p2"><span class="s1">These Terms of Use govern your use of the <b>EC BENTO</b> website and mobile application, unless otherwise stated, to your use of any other website or mobile application owned or operated by<b>EC BENTO</b> or operated on behalf of <b>EC BENTO</b> (collectively the “Websites”). Please read these Terms of Use carefully before using the Websites or mobile application. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Agreement to Terms</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">By using the Websites, you agree to these Terms of Use and the <b>EC BENTO General Online Privacy Policy</b> (for visitors who are eighteen years of age or over) or the <b>EC BENTO Children’s Online Privacy Policy</b> (for visitors who are under eighteen (18) years of age) which are incorporated herein by reference. Each time you use the Websites, you reaffirm your acceptance of the then-current Terms of Use. If you do not wish to be bound by these Terms of Use, your only remedy is to discontinue using the Websites. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>EC BENTO</b> may change these Terms of Use at any time and in its sole discretion without any notice. The modified Terms of Use will be effective immediately upon posting and you agree to the new posted Terms of Use by continuing your use of the Websites. You are responsible for staying informed of any changes. If you do not agree with the modified Terms of Use, your only remedy is to discontinue using the Websites. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Accounts</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">You may be required to create an account and specify a password to use certain features on the Websites. You agree to provide, maintain and update true, accurate, current and complete information about yourself as prompted by the registration processes. You may not impersonate any person or entity or misrepresent your identity or affiliation with any person or entity, including using another person’s username, password, or other account information. </span><span class="s2"><br>
+<br>
+</span><span class="s1">You are entirely responsible for maintaining the confidentiality of your password and your account. And you are entirely responsible for all activity made by you or anyone that uses your account. You agree to safeguard your password from access by others. If you believe that your account has been compromised, you must immediately contact us by mail at: <b>support@ecbento.com</b>. You agree to indemnify and hold harmless<b> EC BENTO </b>for losses incurred by <b>EC BENTO</b> or another party due to someone else using your account as a result of your failure to use reasonable care to safeguard your password. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Cancellation</b> </span><span class="s2"><br>
+</span><span class="s1">- No cancellation will be accepted for the confirmed order unless the user has given certificate of sickness, and shall be made before 8:00AM directly to EC Bento at 96689069 on the day of absence.</span></p>
+<p class="p2"><span class="s1">- No cancellation will be accepted for the confirmed order unless the user has given certificate of sickness, and shall be made before 8:00AM directly to EC Bento at 96689069 on the day of absence.</span></p>
+<p class="p2"><span class="s1">- In case of outbreak affecting lunch arrangement or last-minute school closure, refunds will be given. Provider reserves the right to make the final decisions regarding any refunds.</span></p>
+<p class="p3"><span class="s2"></span><br></p>
+<p class="p2"><span class="s1"><b>Special Arrangements Under Inclement Weather Conditions</b></span></p>
+<p class="p2"><span class="s1">- In the event that Tropical warning Signal No.8 or above is issued before 8:00A.M. on the service day, the Service Provider will suspend to provide the Service to the Client.</span></p>
+<p class="p2"><span class="s1">- If Black Rainstorm Warning is issued before 8:00A.M. on the service day, the Service Provider will suspend to provide the Service to the Client.</span></p>
+<p class="p2"><span class="s1">- If tropical warning Signal No.8 or above or Black Rainstorm Warning is issued during the school hours, the Service Provider will use the best endeavours to maintain the Service.</span></p>
+<p class="p2"><span class="s1">- If the Service Provider cannot provide the Service to the client due to the Inclement Weather Condition, the Service Provider shall immediately inform the Client and all the orders on that day will proceed refund to the users within 2 months.</span></p>
+<p class="p2"><span class="s2"><br>
+</span><span class="s1"><b>Content Posted by Other Users</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>EC BENTO</b> is not responsible for, and does not endorse, Content in any posting made by other users on the Websites. Under no circumstances shall <b>EC BENTO</b> be held liable, directly or indirectly, for any loss or damage caused or alleged to have been caused to you in connection with any Content posted by a third party on the Websites. If you become aware of misuse of the Websites by any person, please contact EC BENTO by mail at: <b>support@ecbento.com.</b> </span><span class="s2"><br>
+</span><span class="s1">If you feel threatened or believe someone else is in danger, you should contact the local law enforcement agency immediately. </span><span class="s2"><br>
+<br>
+<br>
+</span><span class="s1"><b>Activities Prohibited on the Websites</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">The following is a partial list of the kinds of conduct that are illegal or prohibited on the Websites. <b>EC BENTO</b> reserves the right to investigate and take appropriate legal action against anyone who, in <b>EC BENTO</b>'s sole discretion, engages in any of the prohibited activities. Prohibited activities include but are not limited to the following: </span><span class="s2"><br>
+<br>
+</span><span class="s1">- Using the Websites for any purpose in violation of local, state, or federal laws or regulations; </span><span class="s2"><br>
+</span><span class="s1">- Posting Content that infringes the intellectual property rights, privacy rights, publicity rights, trade secret rights, or any other rights of any party; </span><span class="s2"><br>
+</span><span class="s1">- Posting Content that is unlawful, obscene, defamatory, threatening, harassing, abusive, slanderous, hateful, or embarrassing to any other person or entity as determined by <b>EC BENTO</b> in its sole discretion or pursuant to local community standards; </span><span class="s2"><br>
+</span><span class="s1">- Posting Content that constitutes cyber-bullying, as determined by EC BENTO in its sole discretion; </span><span class="s2"><br>
+</span><span class="s1">- Posting Content that depicts any dangerous, life-threatening, or otherwise risky behavior; </span><span class="s2"><br>
+</span><span class="s1">- Posting telephone numbers, street addresses, or last names of any person; </span><span class="s2"><br>
+</span><span class="s1">- Posting URLs to external websites or any form of HTML or programming code; </span><span class="s2"><br>
+</span><span class="s1">- Posting anything that may be “spam,” as determined by EC BENTO in its sole discretion; </span><span class="s2"><br>
+</span><span class="s1">- Impersonating another person when posting Content; </span><span class="s2"><br>
+</span><span class="s1">- Harvesting or otherwise collecting information about others, including e-mail addresses, without their consent; </span><span class="s2"><br>
+</span><span class="s1">- Allowing any other person or entity to use your identification for posting or viewing comments; </span><span class="s2"><br>
+</span><span class="s1">- Harassing, threatening, stalking, or abusing any person; </span><span class="s2"><br>
+</span><span class="s1">- Engaging in any other conduct that restricts or inhibits any other person from using or enjoying the Websites, or which, in the sole discretion of <b>EC BENTO</b>, exposes <b>EC BENTO</b> or any of its customers, suppliers, or any other parties to any liability or detriment of any type; or </span><span class="s2"><br>
+</span><span class="s1">- Encouraging other people to engage in any prohibited activities as described herein. </span><span class="s2"><br>
+</span><span class="s1"><b>EC BENTO</b> reserves the right -- but is not obligated -- to do any or all of the following: </span><span class="s2"><br>
+<br>
+</span><span class="s1">- Investigate an allegation that any Content posted on the Websites or mobile application does not conform to these Terms of Use and determine in its sole discretion to remove or request the removal of the Content; </span><span class="s2"><br>
+</span><span class="s1">- Remove Content which is abusive, illegal, or disruptive, or that otherwise fails to conform with these Terms of Use; </span><span class="s2"><br>
+</span><span class="s1">- Terminate a user’s access to the Websites upon any breach of these Terms of Use; </span><span class="s2"><br>
+</span><span class="s1">- Monitor, edit, or disclose any Content on the Websites; and </span><span class="s2"><br>
+</span><span class="s1">- Edit or delete any Content posted on the Websites, regardless of whether such Content violates these standards. </span><span class="s2"><br>
+</span><span class="s1">-<b> EC BENTO</b> Trademarks and Copyrights </span><span class="s2"><br>
+<br>
+</span><span class="s1">All trademarks, logos, and service marks displayed on the Website are registered and unregistered Trademarks of <b>EC BENTO</b> and/or third parties who have authorized their use (collectively the “Trademarks”) </span><span class="s2"><br>
+<br>
+</span><span class="s1">You may not use, copy, reproduce, republish, upload, post, transmit, distribute, or modify these Trademarks in any way. The use of <b>EC BENTO</b>'s trademarks on any other website is strictly prohibited. All of the materials contained on the Websites are copyrighted except where explicitly noted otherwise. EC BENTO will aggressively enforce its intellectual property rights to the fullest extent of the law, including the seeking of criminal prosecution. <b>EC BENTO</b> neither warrants nor represents that your use of materials displayed on the Websites will not infringe rights of third parties not owned by or affiliated with<b> EC BENTO</b>. Use of any materials on the Websites is at your own risk. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Hyperlinks</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">This Websites may contain hyperlinks to third-party websites or mobile application. <b>EC BENTO</b> does not control or endorse these third-party websites or any goods or services sold on those websites or mobile application. Some of these websites may contain materials that are objectionable, unlawful, or inaccurate. You acknowledge and agree that <b>EC BENTO</b> is not responsible or liable for any Content or other materials on these third party websites.</span></p>
+<p class="p2"><span class="s1"><b>Governing Law and Severability</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">These Terms of Use shall be governed by and construed in accordance with the laws of Hong Kong, without regard to its conflict of laws rules. You expressly agree that the exclusive jurisdiction for any claim or dispute under the Terms of Use and or your use of the Websites or mobile application resides in the courts of Hong Kong, and you further expressly agree to submit to the personal jurisdiction of such courts for the purpose of litigating any such claim or action. </span><span class="s2"><br>
+<br>
+</span><span class="s1">If any provision of these Terms of Use is found to be invalid by any court having competent jurisdiction, the invalidity of such provision shall not affect the validity of the remaining provisions of these Terms of Use, which shall remain in full force and effect. No waiver of any provision in these Terms of Use shall be deemed a further or continuing waiver of such provision or any other provision. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Payment</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">Payments are processed by Asia Pay (HK) Limited, which is the ultimate holding of the local company in Hong Kong. Cross-border surcharges may be applicable. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Visa, Master, Alipay, PayPal.</b> The provider reserves the right to provide other payment methods or to no longer offer certain payment methods. The end customer bindingly chooses the payment method when placing the respective order. Provided that the end customer chooses an online payment method, the payment might be processed by an external payment provider cooperating with the provider. Card data will in this case be stored for future orders by the payment provider, on the condition that the end customer chooses the respective storage of such and hereby gives consent to it. </span><span class="s2"><br>
+</span><span class="s1">Payment under HK$4 will automatically switch to Alipay only. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Warranties</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">The Websites and the Content are provided on an “as is” basis. To the fullest extent permitted by law, <b>EC BENTO</b>, its parent, subsidiaries, and affiliates (the <b>EC BENTO </b>entities), and each of their agents, representatives and service providers, disclaim all warranties, either expressed or implied, statutory or otherwise, including but not limited to the implied warranties of merchantability, non-infringement of third parties’ rights, and fitness for particular purpose. Applicable law may not allow the exclusion of implied warranties, so the above exclusion may not apply to you. The <b>EC BENTO</b> Entities, their agents, representatives and service providers cannot and do not guarantee or warrant that: (a) the Websites or mobile application will be reliable, accurate, complete, or updated on a timely basis; (b) the Websites or mobile application will be free of human and machine errors, omissions, delays, interruptions or losses, including loss of data; (c) any files available for downloading from the Websites will be free of infection by viruses, worms, Trojan horses, or other codes that manifest contaminating or destructive properties; (d) any Content you post on the Websites will remain on the Websites; or (e) the functions or services performed on the Websites will be uninterrupted or error-free or that defects in the Websites will be corrected. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Limitation of Liability</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">The <b>EC BENTO</b> entities, their agents, representatives, and service providers, entire liability and your exclusive remedy with respect to your use of the websites is to discontinue your use of the websites. The <b>EC BENTO</b> entities, their agents, representatives, and service providers shall not be liable for any indirect, special, incidental, consequential, or exemplary damages arising from your use of the websites or for any other claim related in any way to your use of the websites. These exclusions for indirect, special, consequential, and exemplary damages include, without limitation, damages for lost profits, lost data, loss of goodwill, work stoppage, work stoppage, computer failure, or malfunction, or any other commercial damages or losses, even if the <b>EC BNETO</b> entities, their agents, representatives, and service providers have been advised of the possibility thereof and regardless of the legal or equitable theory upon which the claim is based. Because some states or jurisdictions do not allow the exclusion or the limitation of liability for consequential or incidental damages, in such states or jurisdictions, the <b>EC BENTO </b>entities, their agents, representatives and service providers' liability shall be limited to the extent permitted by law. </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Termination</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>EC BENTO</b> reserves the right to terminate your account and access to the Websites for any reason, including, without limitation, if<b> EC BENTO</b>, in its sole discretion, considers your use to be unacceptable, or in the event of any breach by you of the Terms of Use. <b>EC BENTO</b> may, but shall be under no obligation to, provide you a warning prior to termination of your use of the Websites. </span><span class="s2"><br>
+<br>
+</span></p>
+<p class="p2"><span class="s1"><b>Vouchers</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">Unless otherwise stated, </span><span class="s2"><br>
+</span><span class="s1">- Vouchers are only applicable to food orders </span><span class="s2"><br>
+</span><span class="s1">- Valid only for online payment </span><span class="s2"><br>
+</span><span class="s1">- <b>EC BENTO</b> reserves the right to cancel orders and accounts without any prior notice if fraud activities are detected </span><span class="s2"><br>
+</span><span class="s1">- <b>EC BENTO</b> reserves the right to cancel any voucher to be used on placing order on the website without any prior notice..<span class="Apple-converted-space"> </span></span></p>
+<p class="p2"><span class="s1">- 10% Service Charge of any refund or cancellation of account is required.</span><span class="s2"><br>
+<br>
+</span><span class="s1"><b>Contact Us</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">Questions? Comments? Please send an email to us at <b>support@ecbento.com</b> </span><span class="s2"><br>
+<br>
+</span><span class="s1">© 2021 EC BENTO. All rights reserved.</span></p>
+            </section>
 
         </div>
     </main>
