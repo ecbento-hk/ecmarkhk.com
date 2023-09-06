afca85d9592f597f1ca42f61f00b077d7dea6377
Man Fai <manfai106@gmail.com>
2023-09-06 19:06:41 +0800

    hotfix

--- resources/views/livewire/checkout-card.blade.php
+++ resources/views/livewire/checkout-card.blade.php
@@ -105,7 +105,8 @@
               <svg class="w-6 h-6 mx-2 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
               </svg>
-              <label>{{__('Due to the system update of the third-party payment gateway, customers need to input credit card detail again.')}}</label>
+              <!-- <label>{{__('Due to the system update of the third-party payment gateway, customers need to input credit card detail again.')}}</label> -->
+              <label>{{__('Due to updates of payment gateway, please click credit card >  proceed to checkout, system will direct to checkout platform.')}}</label>
             </div>
           </div>
 
