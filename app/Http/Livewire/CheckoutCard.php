dad3ed4d2f22097abea65abdbdd8f32801b74de2
Man Fai <manfai106@gmail.com>
2023-09-06 19:39:38 +0800

    hotfix

--- app/Http/Livewire/CheckoutCard.php
+++ app/Http/Livewire/CheckoutCard.php
@@ -211,7 +211,7 @@ class CheckoutCard extends Component
         $this->loading = true;
         // $this->done = false;
         \Log::channel('order')->info('User Start of Create Order');
-        // try {
+        try {
             if($this->cartItems->count()>0){
 
                 if($this->selected_payment=='new'){
@@ -360,14 +360,16 @@ class CheckoutCard extends Component
                             }
                             $checkout_session = $stripe->checkout->sessions->create([
                                 'line_items' => [
-                                    'price_data' => [
-                                    'currency' => 'hkd',
-                                    'product_data' => [
-                                        'name' => 'Order #'.$order->no
-                                    ],
-                                    'unit_amount' => $amount*100,
-                                    ],
-                                    'quantity' => 1,
+                                    [
+                                        'price_data' => [
+                                        'currency' => 'hkd',
+                                        'product_data' => [
+                                            'name' => 'Order #'.$order->no
+                                        ],
+                                        'unit_amount' => $amount*100,
+                                        ],
+                                        'quantity' => 1,
+                                    ]
                                 ],
                                 'mode' => 'payment',
                                 'success_url' => 'https://school-dsc.ecbento.com/success/'.$order->no,
@@ -466,13 +468,13 @@ class CheckoutCard extends Component
                
 
             }
-        // } catch (\Throwable $th) {
-        //     // dd($th);
-        //     // \Log::debug(auth()->user());
-        //     \Log::channel('order')->debug($th->getMessage());
-        //     session()->flash('message', $th->getMessage());
-        //     $this->emit('$refresh');  
-        // }
+        } catch (\Throwable $th) {
+            // dd($th);
+            // \Log::debug(auth()->user());
+            \Log::channel('order')->debug($th->getMessage());
+            session()->flash('message', $th->getMessage());
+            $this->emit('$refresh');  
+        }
         \Log::channel('order')->info('User End of Create Order');
         $this->procced = 'Proceed to checkout';
         // $this->done = true;
